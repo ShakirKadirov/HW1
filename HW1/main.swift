5d6d9000f591cac473e5965466ca05ecb17f14fb
ShakirKadirov <shakirkadirov1@gmail.com>
2023-11-22 11:09:26 +0600

    Task #1 completed

--- HW1/main.swift
+++ HW1/main.swift
@@ -7,5 +7,19 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: Task1
 
+var condition1 = true
+var condition2 = false
+
+if condition1 && condition2 {
+    print("Оба условия выполняются")
+} else {
+    print("Не все условия выполняются")
+}
+
+if condition1 || condition2 {
+    print("Хотя бы одно из условий выполняется")
+} else {
+    print("Оба условия ложны")
+}
